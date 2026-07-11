9215eb5134424dd068cede31874436c06c1db899
Leonardo Delgado <angel.delgadocar@ug.edu.ec>
2026-07-11 00:05:27 -0500

    Preparar backend para despliegue

--- backend/index.js
+++ backend/index.js
@@ -79,4 +79,8 @@ app.get("/api/users/get-email/:username", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
