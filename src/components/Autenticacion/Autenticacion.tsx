0f999f51cd3c2ea74427fb8507d1783aa647a739
xnjwt <xtrangernowrol@gmail.com>
2026-07-11 19:19:07 -0500

    Proyecto Final 3

--- src/components/Autenticacion/Autenticacion.tsx
+++ src/components/Autenticacion/Autenticacion.tsx
@@ -490,7 +490,7 @@ export const Autenticacion = ({ onAuthSuccess }: FormularioAccesoProps) => {
               color: 'primary.main',
               backgroundColor: theme.palette.mode === 'dark' ? 'rgba(255,255,255,0.05)' : 'rgba(0,0,0,0.03)',
             },
-          }}
+          }}  
         >
           {esModoLogin ? t.irARegistro : t.irALogin}
         </Button>
